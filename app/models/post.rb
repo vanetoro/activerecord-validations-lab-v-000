202a506e978adafedebd7a9672f45cbca15434b9
Vanessa Toro <vanetoro84@gmail.com>
2018-07-03 19:07:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,19 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+  clickbait = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top[0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if clickbait.none?{|pat| pat.match title}
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
